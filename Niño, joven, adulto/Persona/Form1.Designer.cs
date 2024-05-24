87f11bdd2d3c218e35af8c7eb4bf4d8593c042e7
Park Jihoon <trabajostodo0@gmail.com>
2024-05-23 19:48:29 -0600

    Agregar archivos de proyecto.

---
+++ Niño, joven, adulto/Persona/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Persona
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.btncerrar = new System.Windows.Forms.Button();
+            this.btncalc = new System.Windows.Forms.Button();
+            this.btnlimpiar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtedad = new System.Windows.Forms.TextBox();
+            this.txtperso = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(70, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(142, 22);
+            this.label3.TabIndex = 32;
+            this.label3.Text = "Tipo de persona:";
+            // 
+            // btncerrar
+            // 
+            this.btncerrar.BackColor = System.Drawing.Color.LavenderBlush;
+            this.btncerrar.Font = new System.Drawing.Font("Constantia", 12.22642F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncerrar.Location = new System.Drawing.Point(317, 194);
+            this.btncerrar.Name = "btncerrar";
+            this.btncerrar.Size = new System.Drawing.Size(138, 56);
+            this.btncerrar.TabIndex = 31;
+            this.btncerrar.Text = "Cerrar";
+            this.btncerrar.UseVisualStyleBackColor = false;
+            this.btncerrar.Click += new System.EventHandler(this.btncerrar_Click);
+            // 
+            // btncalc
+            // 
+            this.btncalc.BackColor = System.Drawing.Color.LavenderBlush;
+            this.btncalc.Font = new System.Drawing.Font("Constantia", 12.22642F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncalc.Location = new System.Drawing.Point(173, 194);
+            this.btncalc.Name = "btncalc";
+            this.btncalc.Size = new System.Drawing.Size(138, 56);
+            this.btncalc.TabIndex = 30;
+            this.btncalc.Text = "Calcular";
+            this.btncalc.UseVisualStyleBackColor = false;
+            this.btncalc.Click += new System.EventHandler(this.btncalc_Click);
+            // 
+            // btnlimpiar
+            // 
+            this.btnlimpiar.BackColor = System.Drawing.Color.LavenderBlush;
+            this.btnlimpiar.Font = new System.Drawing.Font("Constantia", 12.22642F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnlimpiar.Location = new System.Drawing.Point(29, 194);
+            this.btnlimpiar.Name = "btnlimpiar";
+            this.btnlimpiar.Size = new System.Drawing.Size(138, 56);
+            this.btnlimpiar.TabIndex = 29;
+            this.btnlimpiar.Text = "Limpiar";
+            this.btnlimpiar.UseVisualStyleBackColor = false;
+            this.btnlimpiar.Click += new System.EventHandler(this.btnlimpiar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Constantia", 12.22642F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(151, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 23);
+            this.label1.TabIndex = 28;
+            this.label1.Text = "Escriba su edad:";
+            // 
+            // txtedad
+            // 
+            this.txtedad.Location = new System.Drawing.Point(161, 75);
+            this.txtedad.Name = "txtedad";
+            this.txtedad.Size = new System.Drawing.Size(137, 29);
+            this.txtedad.TabIndex = 27;
+            this.txtedad.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtperso
+            // 
+            this.txtperso.Location = new System.Drawing.Point(224, 130);
+            this.txtperso.Name = "txtperso";
+            this.txtperso.Size = new System.Drawing.Size(143, 29);
+            this.txtperso.TabIndex = 26;
+            this.txtperso.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lavender;
+            this.ClientSize = new System.Drawing.Size(484, 278);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btncerrar);
+            this.Controls.Add(this.btncalc);
+            this.Controls.Add(this.btnlimpiar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtedad);
+            this.Controls.Add(this.txtperso);
+            this.Font = new System.Drawing.Font("Constantia", 12.22642F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btncerrar;
+        private System.Windows.Forms.Button btncalc;
+        private System.Windows.Forms.Button btnlimpiar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtedad;
+        private System.Windows.Forms.TextBox txtperso;
+    }
+}
+
---
+++ Niño, joven, adulto/Persona/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Persona
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btncalc_Click(object sender, EventArgs e)
+        {
+            int edad;
+            String persona;
+
+            if (string.IsNullOrEmpty(txtedad.Text))
+            {
+                MessageBox.Show("Por favor, ingresa un valor en los campos en blanco", "Dato faltante", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                edad = Convert.ToInt32(txtedad.Text);
+
+                if (edad < 13)
+                {
+                    persona = "Niño";
+                    txtperso.Text = persona.ToString();
+                }
+                else if (edad >= 13 && edad < 25)
+                {
+                    persona = "Joven";
+                    txtperso.Text = persona.ToString();
+                }
+                else if (edad >= 25)
+                {
+                    persona = "Adulto";
+                    txtperso.Text = persona.ToString();
+                }
+            }
+        }
+
+        private void btnlimpiar_Click(object sender, EventArgs e)
+        {
+            txtedad.Clear();
+            txtperso.Clear();
+        }
+
+        private void btncerrar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
