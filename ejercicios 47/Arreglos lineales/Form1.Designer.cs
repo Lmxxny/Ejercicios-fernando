d64f3b2da8caca776af6a8fc08128eff0297fc81
Park Jihoon <trabajostodo0@gmail.com>
2024-05-24 18:13:59 -0600

    ejercicio 47

---
+++ ejercicios 47/Arreglos lineales/Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace Arreglos_lineales
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btncerrar = new System.Windows.Forms.Button();
+            this.btncalc = new System.Windows.Forms.Button();
+            this.btnlimpiar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txta1 = new System.Windows.Forms.TextBox();
+            this.txtorto = new System.Windows.Forms.TextBox();
+            this.txtprodpun = new System.Windows.Forms.TextBox();
+            this.txtb1 = new System.Windows.Forms.TextBox();
+            this.txta2 = new System.Windows.Forms.TextBox();
+            this.txta3 = new System.Windows.Forms.TextBox();
+            this.txtb2 = new System.Windows.Forms.TextBox();
+            this.txtb3 = new System.Windows.Forms.TextBox();
+            this.txtz = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btncerrar
+            // 
+            this.btncerrar.BackColor = System.Drawing.Color.LavenderBlush;
+            this.btncerrar.Font = new System.Drawing.Font("Constantia", 12.22642F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncerrar.Location = new System.Drawing.Point(365, 387);
+            this.btncerrar.Name = "btncerrar";
+            this.btncerrar.Size = new System.Drawing.Size(138, 56);
+            this.btncerrar.TabIndex = 19;
+            this.btncerrar.Text = "Cerrar";
+            this.btncerrar.UseVisualStyleBackColor = false;
+            this.btncerrar.Click += new System.EventHandler(this.btncerrar_Click);
+            // 
+            // btncalc
+            // 
+            this.btncalc.BackColor = System.Drawing.Color.LavenderBlush;
+            this.btncalc.Font = new System.Drawing.Font("Constantia", 12.22642F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncalc.Location = new System.Drawing.Point(221, 387);
+            this.btncalc.Name = "btncalc";
+            this.btncalc.Size = new System.Drawing.Size(138, 56);
+            this.btncalc.TabIndex = 18;
+            this.btncalc.Text = "Calcular";
+            this.btncalc.UseVisualStyleBackColor = false;
+            this.btncalc.Click += new System.EventHandler(this.btncalc_Click);
+            // 
+            // btnlimpiar
+            // 
+            this.btnlimpiar.BackColor = System.Drawing.Color.LavenderBlush;
+            this.btnlimpiar.Font = new System.Drawing.Font("Constantia", 12.22642F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnlimpiar.Location = new System.Drawing.Point(77, 387);
+            this.btnlimpiar.Name = "btnlimpiar";
+            this.btnlimpiar.Size = new System.Drawing.Size(138, 56);
+            this.btnlimpiar.TabIndex = 17;
+            this.btnlimpiar.Text = "Limpiar";
+            this.btnlimpiar.UseVisualStyleBackColor = false;
+            this.btnlimpiar.Click += new System.EventHandler(this.btnlimpiar_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Constantia", 12.22642F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(274, 176);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 23);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "A:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Constantia", 12.22642F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(229, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Convertir:";
+            // 
+            // txta1
+            // 
+            this.txta1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.txta1.Location = new System.Drawing.Point(115, 80);
+            this.txta1.Name = "txta1";
+            this.txta1.Size = new System.Drawing.Size(82, 21);
+            this.txta1.TabIndex = 14;
+            this.txta1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtorto
+            // 
+            this.txtorto.Location = new System.Drawing.Point(330, 268);
+            this.txtorto.Name = "txtorto";
+            this.txtorto.Size = new System.Drawing.Size(193, 29);
+            this.txtorto.TabIndex = 13;
+            this.txtorto.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtprodpun
+            // 
+            this.txtprodpun.Location = new System.Drawing.Point(330, 211);
+            this.txtprodpun.Name = "txtprodpun";
+            this.txtprodpun.Size = new System.Drawing.Size(193, 29);
+            this.txtprodpun.TabIndex = 12;
+            this.txtprodpun.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtb1
+            // 
+            this.txtb1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.txtb1.Location = new System.Drawing.Point(115, 133);
+            this.txtb1.Name = "txtb1";
+            this.txtb1.Size = new System.Drawing.Size(82, 21);
+            this.txtb1.TabIndex = 24;
+            this.txtb1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txta2
+            // 
+            this.txta2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.txta2.Location = new System.Drawing.Point(240, 80);
+            this.txta2.Name = "txta2";
+            this.txta2.Size = new System.Drawing.Size(82, 21);
+            this.txta2.TabIndex = 25;
+            this.txta2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txta3
+            // 
+            this.txta3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.txta3.Location = new System.Drawing.Point(365, 80);
+            this.txta3.Name = "txta3";
+            this.txta3.Size = new System.Drawing.Size(82, 21);
+            this.txta3.TabIndex = 26;
+            this.txta3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtb2
+            // 
+            this.txtb2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.txtb2.Location = new System.Drawing.Point(240, 133);
+            this.txtb2.Name = "txtb2";
+            this.txtb2.Size = new System.Drawing.Size(82, 21);
+            this.txtb2.TabIndex = 27;
+            this.txtb2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtb3
+            // 
+            this.txtb3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.txtb3.Location = new System.Drawing.Point(365, 133);
+            this.txtb3.Name = "txtb3";
+            this.txtb3.Size = new System.Drawing.Size(82, 21);
+            this.txtb3.TabIndex = 28;
+            this.txtb3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtz
+            // 
+            this.txtz.Location = new System.Drawing.Point(330, 328);
+            this.txtz.Name = "txtz";
+            this.txtz.Size = new System.Drawing.Size(193, 29);
+            this.txtz.TabIndex = 29;
+            this.txtz.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(91, 214);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(137, 22);
+            this.label3.TabIndex = 30;
+            this.label3.Text = "Producto Punto:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(91, 271);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(128, 22);
+            this.label4.TabIndex = 31;
+            this.label4.Text = "¿Es Ortogonal?";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 331);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(290, 22);
+            this.label5.TabIndex = 32;
+            this.label5.Text = "Producto puntos/producto módulos";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Constantia", 12.22642F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(73, 131);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(28, 23);
+            this.label6.TabIndex = 33;
+            this.label6.Text = "B:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Constantia", 12.22642F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(73, 78);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(28, 23);
+            this.label7.TabIndex = 34;
+            this.label7.Text = "A:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lavender;
+            this.ClientSize = new System.Drawing.Size(547, 469);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtz);
+            this.Controls.Add(this.txtb3);
+            this.Controls.Add(this.txtb2);
+            this.Controls.Add(this.txta3);
+            this.Controls.Add(this.txta2);
+            this.Controls.Add(this.txtb1);
+            this.Controls.Add(this.btncerrar);
+            this.Controls.Add(this.btncalc);
+            this.Controls.Add(this.btnlimpiar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txta1);
+            this.Controls.Add(this.txtorto);
+            this.Controls.Add(this.txtprodpun);
+            this.Font = new System.Drawing.Font("Constantia", 12.22642F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btncerrar;
+        private System.Windows.Forms.Button btncalc;
+        private System.Windows.Forms.Button btnlimpiar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txta1;
+        private System.Windows.Forms.TextBox txtorto;
+        private System.Windows.Forms.TextBox txtprodpun;
+        private System.Windows.Forms.TextBox txtb1;
+        private System.Windows.Forms.TextBox txta2;
+        private System.Windows.Forms.TextBox txta3;
+        private System.Windows.Forms.TextBox txtb2;
+        private System.Windows.Forms.TextBox txtb3;
+        private System.Windows.Forms.TextBox txtz;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ ejercicios 47/Arreglos lineales/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Arreglos_lineales
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnlimpiar_Click(object sender, EventArgs e)
+        {
+            txta1.Clear();
+            txta2.Clear();
+            txta3.Clear();
+            txtb1.Clear();
+            txtb2.Clear();
+            txtb3.Clear();
+            txtorto.Clear();
+            txtprodpun.Clear();
+            txtz.Clear();
+        }
+
+        private void btncerrar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btncalc_Click(object sender, EventArgs e)
+        {
+            double a1, a2, a3;
+            double b1, b2, b3;
+
+            if (string.IsNullOrEmpty(txta1.Text) || string.IsNullOrEmpty(txta2.Text) || string.IsNullOrEmpty(txta3.Text) || string.IsNullOrEmpty(txtb1.Text) || string.IsNullOrEmpty(txtb2.Text) || string.IsNullOrEmpty(txtb3.Text))
+            {
+                MessageBox.Show("Por favor, ingresa un valor en los campos en blanco", "Dato faltante", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                a1 = double.Parse(txta1.Text);
+                a2 = double.Parse(txta2.Text);
+                a3 = double.Parse(txta3.Text);
+                b1 = double.Parse(txtb1.Text);
+                b2 = double.Parse(txtb2.Text);
+                b3 = double.Parse(txtb3.Text);
+
+                //producto punto
+                double productoPunto = a1 * b1 + a2 * b2 + a3 * b3;
+                txtprodpun.Text = productoPunto.ToString();
+
+                //ortogonalidad
+                const double tolerancia = 1e-10;
+                bool sonOrtogonales = Math.Abs(productoPunto) < tolerancia;
+                string ortogonal;
+
+                if (sonOrtogonales)
+                {
+                    ortogonal = "Son ortogonales.";
+                    txtorto.Text = ortogonal;
+                }
+                else
+                {
+                    ortogonal = "No son ortogonales.";
+                    txtorto.Text = ortogonal;
+                }
+
+                //Calcular Z
+                double moduloA = Math.Sqrt(a1 * a1 + a2 * a2 + a3 * a3);
+                double moduloB = Math.Sqrt(b1 * b1 + b2 * b2 + b3 * b3);
+
+                double Z = productoPunto / (moduloA * moduloB);
+                txtz.Text = Z.ToString();
+            }
+        }
+    }
+}
