1bd6a7aa1c00dbc362da215d56d95f5ea237b815
Park Jihoon <trabajostodo0@gmail.com>
2024-05-24 19:01:02 -0600

    ejercicio 49

---
+++ ejercicio 49/ejercicio 49/Form1.Designer.cs
@@ -0,0 +1,399 @@
+﻿namespace ejercicio_49
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtb3 = new System.Windows.Forms.TextBox();
+            this.txtb2 = new System.Windows.Forms.TextBox();
+            this.txta3 = new System.Windows.Forms.TextBox();
+            this.txta2 = new System.Windows.Forms.TextBox();
+            this.txtb1 = new System.Windows.Forms.TextBox();
+            this.btncerrar = new System.Windows.Forms.Button();
+            this.btncalc = new System.Windows.Forms.Button();
+            this.btnlimpiar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txta1 = new System.Windows.Forms.TextBox();
+            this.txt2a3b = new System.Windows.Forms.TextBox();
+            this.txtsumab = new System.Windows.Forms.TextBox();
+            this.txtc3 = new System.Windows.Forms.TextBox();
+            this.txtc2 = new System.Windows.Forms.TextBox();
+            this.txtc1 = new System.Windows.Forms.TextBox();
+            this.txtc6 = new System.Windows.Forms.TextBox();
+            this.txtc5 = new System.Windows.Forms.TextBox();
+            this.txtc4 = new System.Windows.Forms.TextBox();
+            this.txtb6 = new System.Windows.Forms.TextBox();
+            this.txtb5 = new System.Windows.Forms.TextBox();
+            this.txta6 = new System.Windows.Forms.TextBox();
+            this.txta5 = new System.Windows.Forms.TextBox();
+            this.txtb4 = new System.Windows.Forms.TextBox();
+            this.txta4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Constantia", 12.22642F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(228, 45);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(28, 23);
+            this.label7.TabIndex = 51;
+            this.label7.Text = "A:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Constantia", 12.22642F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(715, 45);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(28, 23);
+            this.label6.TabIndex = 50;
+            this.label6.Text = "B:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(283, 377);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(153, 23);
+            this.label4.TabIndex = 49;
+            this.label4.Text = "Suma de 2A + 3B:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(306, 320);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 23);
+            this.label3.TabIndex = 48;
+            this.label3.Text = "Suma A + B:";
+            // 
+            // txtb3
+            // 
+            this.txtb3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.txtb3.Location = new System.Drawing.Point(331, 159);
+            this.txtb3.Name = "txtb3";
+            this.txtb3.Size = new System.Drawing.Size(82, 21);
+            this.txtb3.TabIndex = 47;
+            this.txtb3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtb2
+            // 
+            this.txtb2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.txtb2.Location = new System.Drawing.Point(206, 159);
+            this.txtb2.Name = "txtb2";
+            this.txtb2.Size = new System.Drawing.Size(82, 21);
+            this.txtb2.TabIndex = 46;
+            this.txtb2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txta3
+            // 
+            this.txta3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.txta3.Location = new System.Drawing.Point(331, 106);
+            this.txta3.Name = "txta3";
+            this.txta3.Size = new System.Drawing.Size(82, 21);
+            this.txta3.TabIndex = 45;
+            this.txta3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txta2
+            // 
+            this.txta2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.txta2.Location = new System.Drawing.Point(206, 106);
+            this.txta2.Name = "txta2";
+            this.txta2.Size = new System.Drawing.Size(82, 21);
+            this.txta2.TabIndex = 44;
+            this.txta2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtb1
+            // 
+            this.txtb1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.txtb1.Location = new System.Drawing.Point(81, 159);
+            this.txtb1.Name = "txtb1";
+            this.txtb1.Size = new System.Drawing.Size(82, 21);
+            this.txtb1.TabIndex = 43;
+            this.txtb1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btncerrar
+            // 
+            this.btncerrar.BackColor = System.Drawing.Color.LavenderBlush;
+            this.btncerrar.Font = new System.Drawing.Font("Constantia", 12.22642F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncerrar.Location = new System.Drawing.Point(549, 436);
+            this.btncerrar.Name = "btncerrar";
+            this.btncerrar.Size = new System.Drawing.Size(138, 56);
+            this.btncerrar.TabIndex = 42;
+            this.btncerrar.Text = "Cerrar";
+            this.btncerrar.UseVisualStyleBackColor = false;
+            this.btncerrar.Click += new System.EventHandler(this.btncerrar_Click);
+            // 
+            // btncalc
+            // 
+            this.btncalc.BackColor = System.Drawing.Color.LavenderBlush;
+            this.btncalc.Font = new System.Drawing.Font("Constantia", 12.22642F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncalc.Location = new System.Drawing.Point(405, 436);
+            this.btncalc.Name = "btncalc";
+            this.btncalc.Size = new System.Drawing.Size(138, 56);
+            this.btncalc.TabIndex = 41;
+            this.btncalc.Text = "Calcular";
+            this.btncalc.UseVisualStyleBackColor = false;
+            this.btncalc.Click += new System.EventHandler(this.btncalc_Click);
+            // 
+            // btnlimpiar
+            // 
+            this.btnlimpiar.BackColor = System.Drawing.Color.LavenderBlush;
+            this.btnlimpiar.Font = new System.Drawing.Font("Constantia", 12.22642F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnlimpiar.Location = new System.Drawing.Point(261, 436);
+            this.btnlimpiar.Name = "btnlimpiar";
+            this.btnlimpiar.Size = new System.Drawing.Size(138, 56);
+            this.btnlimpiar.TabIndex = 40;
+            this.btnlimpiar.Text = "Limpiar";
+            this.btnlimpiar.UseVisualStyleBackColor = false;
+            this.btnlimpiar.Click += new System.EventHandler(this.btnlimpiar_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Constantia", 12.22642F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(468, 282);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 23);
+            this.label2.TabIndex = 39;
+            this.label2.Text = "A:";
+            // 
+            // txta1
+            // 
+            this.txta1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.txta1.Location = new System.Drawing.Point(81, 106);
+            this.txta1.Name = "txta1";
+            this.txta1.Size = new System.Drawing.Size(82, 21);
+            this.txta1.TabIndex = 37;
+            this.txta1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt2a3b
+            // 
+            this.txt2a3b.Location = new System.Drawing.Point(448, 374);
+            this.txt2a3b.Name = "txt2a3b";
+            this.txt2a3b.Size = new System.Drawing.Size(193, 29);
+            this.txt2a3b.TabIndex = 36;
+            this.txt2a3b.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtsumab
+            // 
+            this.txtsumab.Location = new System.Drawing.Point(448, 317);
+            this.txtsumab.Name = "txtsumab";
+            this.txtsumab.Size = new System.Drawing.Size(193, 29);
+            this.txtsumab.TabIndex = 35;
+            this.txtsumab.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtc3
+            // 
+            this.txtc3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.txtc3.Location = new System.Drawing.Point(331, 217);
+            this.txtc3.Name = "txtc3";
+            this.txtc3.Size = new System.Drawing.Size(82, 21);
+            this.txtc3.TabIndex = 54;
+            this.txtc3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtc2
+            // 
+            this.txtc2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.txtc2.Location = new System.Drawing.Point(206, 217);
+            this.txtc2.Name = "txtc2";
+            this.txtc2.Size = new System.Drawing.Size(82, 21);
+            this.txtc2.TabIndex = 53;
+            this.txtc2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtc1
+            // 
+            this.txtc1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.txtc1.Location = new System.Drawing.Point(81, 217);
+            this.txtc1.Name = "txtc1";
+            this.txtc1.Size = new System.Drawing.Size(82, 21);
+            this.txtc1.TabIndex = 52;
+            this.txtc1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtc6
+            // 
+            this.txtc6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.txtc6.Location = new System.Drawing.Point(814, 217);
+            this.txtc6.Name = "txtc6";
+            this.txtc6.Size = new System.Drawing.Size(82, 21);
+            this.txtc6.TabIndex = 63;
+            this.txtc6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtc5
+            // 
+            this.txtc5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.txtc5.Location = new System.Drawing.Point(689, 217);
+            this.txtc5.Name = "txtc5";
+            this.txtc5.Size = new System.Drawing.Size(82, 21);
+            this.txtc5.TabIndex = 62;
+            this.txtc5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtc4
+            // 
+            this.txtc4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.txtc4.Location = new System.Drawing.Point(564, 217);
+            this.txtc4.Name = "txtc4";
+            this.txtc4.Size = new System.Drawing.Size(82, 21);
+            this.txtc4.TabIndex = 61;
+            this.txtc4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtb6
+            // 
+            this.txtb6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.txtb6.Location = new System.Drawing.Point(814, 159);
+            this.txtb6.Name = "txtb6";
+            this.txtb6.Size = new System.Drawing.Size(82, 21);
+            this.txtb6.TabIndex = 60;
+            this.txtb6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtb5
+            // 
+            this.txtb5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.txtb5.Location = new System.Drawing.Point(689, 159);
+            this.txtb5.Name = "txtb5";
+            this.txtb5.Size = new System.Drawing.Size(82, 21);
+            this.txtb5.TabIndex = 59;
+            this.txtb5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txta6
+            // 
+            this.txta6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.txta6.Location = new System.Drawing.Point(814, 106);
+            this.txta6.Name = "txta6";
+            this.txta6.Size = new System.Drawing.Size(82, 21);
+            this.txta6.TabIndex = 58;
+            this.txta6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txta5
+            // 
+            this.txta5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.txta5.Location = new System.Drawing.Point(689, 106);
+            this.txta5.Name = "txta5";
+            this.txta5.Size = new System.Drawing.Size(82, 21);
+            this.txta5.TabIndex = 57;
+            this.txta5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtb4
+            // 
+            this.txtb4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.txtb4.Location = new System.Drawing.Point(564, 159);
+            this.txtb4.Name = "txtb4";
+            this.txtb4.Size = new System.Drawing.Size(82, 21);
+            this.txtb4.TabIndex = 56;
+            this.txtb4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txta4
+            // 
+            this.txta4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.txta4.Location = new System.Drawing.Point(564, 106);
+            this.txta4.Name = "txta4";
+            this.txta4.Size = new System.Drawing.Size(82, 21);
+            this.txta4.TabIndex = 55;
+            this.txta4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lavender;
+            this.ClientSize = new System.Drawing.Size(956, 522);
+            this.Controls.Add(this.txtc6);
+            this.Controls.Add(this.txtc5);
+            this.Controls.Add(this.txtc4);
+            this.Controls.Add(this.txtb6);
+            this.Controls.Add(this.txtb5);
+            this.Controls.Add(this.txta6);
+            this.Controls.Add(this.txta5);
+            this.Controls.Add(this.txtb4);
+            this.Controls.Add(this.txta4);
+            this.Controls.Add(this.txtc3);
+            this.Controls.Add(this.txtc2);
+            this.Controls.Add(this.txtc1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtb3);
+            this.Controls.Add(this.txtb2);
+            this.Controls.Add(this.txta3);
+            this.Controls.Add(this.txta2);
+            this.Controls.Add(this.txtb1);
+            this.Controls.Add(this.btncerrar);
+            this.Controls.Add(this.btncalc);
+            this.Controls.Add(this.btnlimpiar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txta1);
+            this.Controls.Add(this.txt2a3b);
+            this.Controls.Add(this.txtsumab);
+            this.Font = new System.Drawing.Font("Constantia", 12.22642F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtb3;
+        private System.Windows.Forms.TextBox txtb2;
+        private System.Windows.Forms.TextBox txta3;
+        private System.Windows.Forms.TextBox txta2;
+        private System.Windows.Forms.TextBox txtb1;
+        private System.Windows.Forms.Button btncerrar;
+        private System.Windows.Forms.Button btncalc;
+        private System.Windows.Forms.Button btnlimpiar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txta1;
+        private System.Windows.Forms.TextBox txt2a3b;
+        private System.Windows.Forms.TextBox txtsumab;
+        private System.Windows.Forms.TextBox txtc3;
+        private System.Windows.Forms.TextBox txtc2;
+        private System.Windows.Forms.TextBox txtc1;
+        private System.Windows.Forms.TextBox txtc6;
+        private System.Windows.Forms.TextBox txtc5;
+        private System.Windows.Forms.TextBox txtc4;
+        private System.Windows.Forms.TextBox txtb6;
+        private System.Windows.Forms.TextBox txtb5;
+        private System.Windows.Forms.TextBox txta6;
+        private System.Windows.Forms.TextBox txta5;
+        private System.Windows.Forms.TextBox txtb4;
+        private System.Windows.Forms.TextBox txta4;
+    }
+}
+
---
+++ ejercicio 49/ejercicio 49/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ejercicio_49
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txt2a3b.Clear();
+            txta1.Clear();
+            txta2.Clear();
+            txta3.Clear();
+            txta4.Clear();
+            txta5.Clear();
+            txta6.Clear();
+            txtb1.Clear();
+            txtb2.Clear();
+            txtb3.Clear();
+            txtb4.Clear();
+            txtb5.Clear();
+            txtb6.Clear();
+            txtc1.Clear();
+            txtc2.Clear();
+            txtc3.Clear();
+            txtc4.Clear();
+            txtc5.Clear();
+            txtc6.Clear();
+            txtsumab.Clear();
+        }
+
+
+        private void btnlimpiar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btncerrar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btncalc_Click(object sender, EventArgs e)
+        {
+            double a1, a2, a3, a4,a5, a6;
+            double b1, b2, b3, b4, b5, b6;
+            double c1, c2, c3, c4, c5, c6;
+
+            if (string.IsNullOrEmpty(txta1.Text) || string.IsNullOrEmpty(txta2.Text) || string.IsNullOrEmpty(txta3.Text) || string.IsNullOrEmpty(txta4.Text) || string.IsNullOrEmpty(txta5.Text) || string.IsNullOrEmpty(txta6.Text) || string.IsNullOrEmpty(txtb1.Text) || string.IsNullOrEmpty(txtb2.Text) || string.IsNullOrEmpty(txtb3.Text) || string.IsNullOrEmpty(txtb4.Text) || string.IsNullOrEmpty(txtb5.Text) || string.IsNullOrEmpty(txtb6.Text) || string.IsNullOrEmpty(txtc1.Text) || string.IsNullOrEmpty(txtc2.Text) || string.IsNullOrEmpty(txtc3.Text) || string.IsNullOrEmpty(txtc4.Text) || string.IsNullOrEmpty(txtc5.Text) || string.IsNullOrEmpty(txtc6.Text))
+            {
+                MessageBox.Show("Por favor, ingresa un valor en los campos en blanco", "Dato faltante", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                a1 = double.Parse(txta1.Text);
+                a2 = double.Parse(txta2.Text);
+                a3 = double.Parse(txta3.Text);
+                a4 = double.Parse(txta4.Text);
+                a5 = double.Parse(txta5.Text);
+                a6 = double.Parse(txta6.Text);
+                b1 = double.Parse(txtb1.Text);
+                b2 = double.Parse(txtb2.Text);
+                b3 = double.Parse(txtb3.Text);
+                b4 = double.Parse(txtb4.Text);
+                b5 = double.Parse(txtb5.Text);
+                b6 = double.Parse(txtb6.Text);
+                c1 = double.Parse(txtc1.Text);
+                c2 = double.Parse(txtc2.Text);
+                c3 = double.Parse(txtc3.Text);
+                c4 = double.Parse(txtc4.Text);
+                c5 = double.Parse(txtc5.Text);
+                c6 = double.Parse(txtc6.Text);
+
+                //suma a + b
+                double[,] A = new double[,]
+                {
+                    { a1, a2, a3 },
+                    { b1, b2, b3 },
+                    { c1, c2, c3 }
+                };
+
+                double[,] B = new double[,]
+                {
+                    { a4, a5, a6 },
+                    { b4, b5, b6 },
+                    { c4, c5, c6 }
+                };
+
+                double[,] C = new double[3, 3];
+
+                // Calcular la suma de A y B
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        C[i, j] = A[i, j] + B[i, j];
+                    }
+                }
+            }
+        }
+    }
+}
