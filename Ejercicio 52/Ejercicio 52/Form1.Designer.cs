39acda8df711b392bca460b4c9f0f023c2690994
Park Jihoon <trabajostodo0@gmail.com>
2024-05-24 19:42:52 -0600

    Ejercicio 52

---
+++ Ejercicio 52/Ejercicio 52/Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace Ejercicio_52
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btncerrar = new System.Windows.Forms.Button();
+            this.btncalc = new System.Windows.Forms.Button();
+            this.btnlimpiar = new System.Windows.Forms.Button();
+            this.txtcero = new System.Windows.Forms.TextBox();
+            this.txtnegat = new System.Windows.Forms.TextBox();
+            this.txt6al10 = new System.Windows.Forms.TextBox();
+            this.txt1al5 = new System.Windows.Forms.TextBox();
+            this.txt16al20 = new System.Windows.Forms.TextBox();
+            this.txt11al15 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtposit = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Constantia", 12.22642F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(110, 408);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(200, 22);
+            this.label4.TabIndex = 61;
+            this.label4.Text = "Números igual a Cero:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Constantia", 12.22642F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(131, 351);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(181, 22);
+            this.label3.TabIndex = 60;
+            this.label3.Text = "Números negativos:";
+            // 
+            // btncerrar
+            // 
+            this.btncerrar.BackColor = System.Drawing.Color.LavenderBlush;
+            this.btncerrar.Font = new System.Drawing.Font("Constantia", 12.22642F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncerrar.Location = new System.Drawing.Point(429, 467);
+            this.btncerrar.Name = "btncerrar";
+            this.btncerrar.Size = new System.Drawing.Size(138, 56);
+            this.btncerrar.TabIndex = 58;
+            this.btncerrar.Text = "Cerrar";
+            this.btncerrar.UseVisualStyleBackColor = false;
+            this.btncerrar.Click += new System.EventHandler(this.btncerrar_Click);
+            // 
+            // btncalc
+            // 
+            this.btncalc.BackColor = System.Drawing.Color.LavenderBlush;
+            this.btncalc.Font = new System.Drawing.Font("Constantia", 12.22642F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncalc.Location = new System.Drawing.Point(285, 467);
+            this.btncalc.Name = "btncalc";
+            this.btncalc.Size = new System.Drawing.Size(138, 56);
+            this.btncalc.TabIndex = 57;
+            this.btncalc.Text = "Calcular";
+            this.btncalc.UseVisualStyleBackColor = false;
+            this.btncalc.Click += new System.EventHandler(this.btncalc_Click);
+            // 
+            // btnlimpiar
+            // 
+            this.btnlimpiar.BackColor = System.Drawing.Color.LavenderBlush;
+            this.btnlimpiar.Font = new System.Drawing.Font("Constantia", 12.22642F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnlimpiar.Location = new System.Drawing.Point(141, 467);
+            this.btnlimpiar.Name = "btnlimpiar";
+            this.btnlimpiar.Size = new System.Drawing.Size(138, 56);
+            this.btnlimpiar.TabIndex = 56;
+            this.btnlimpiar.Text = "Limpiar";
+            this.btnlimpiar.UseVisualStyleBackColor = false;
+            this.btnlimpiar.Click += new System.EventHandler(this.btnlimpiar_Click);
+            // 
+            // txtcero
+            // 
+            this.txtcero.Location = new System.Drawing.Point(328, 405);
+            this.txtcero.Name = "txtcero";
+            this.txtcero.Size = new System.Drawing.Size(193, 29);
+            this.txtcero.TabIndex = 54;
+            this.txtcero.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtnegat
+            // 
+            this.txtnegat.Location = new System.Drawing.Point(328, 348);
+            this.txtnegat.Name = "txtnegat";
+            this.txtnegat.Size = new System.Drawing.Size(193, 29);
+            this.txtnegat.TabIndex = 53;
+            this.txtnegat.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt6al10
+            // 
+            this.txt6al10.Location = new System.Drawing.Point(328, 125);
+            this.txt6al10.Name = "txt6al10";
+            this.txt6al10.Size = new System.Drawing.Size(193, 29);
+            this.txt6al10.TabIndex = 63;
+            this.txt6al10.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt1al5
+            // 
+            this.txt1al5.Location = new System.Drawing.Point(328, 68);
+            this.txt1al5.Name = "txt1al5";
+            this.txt1al5.Size = new System.Drawing.Size(193, 29);
+            this.txt1al5.TabIndex = 62;
+            this.txt1al5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt16al20
+            // 
+            this.txt16al20.Location = new System.Drawing.Point(328, 247);
+            this.txt16al20.Name = "txt16al20";
+            this.txt16al20.Size = new System.Drawing.Size(193, 29);
+            this.txt16al20.TabIndex = 65;
+            this.txt16al20.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt11al15
+            // 
+            this.txt11al15.Location = new System.Drawing.Point(328, 190);
+            this.txt11al15.Name = "txt11al15";
+            this.txt11al15.Size = new System.Drawing.Size(193, 29);
+            this.txt11al15.TabIndex = 64;
+            this.txt11al15.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Constantia", 12.22642F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(136, 301);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(175, 22);
+            this.label1.TabIndex = 66;
+            this.label1.Text = "Números positivos:";
+            // 
+            // txtposit
+            // 
+            this.txtposit.Location = new System.Drawing.Point(328, 298);
+            this.txtposit.Name = "txtposit";
+            this.txtposit.Size = new System.Drawing.Size(193, 29);
+            this.txtposit.TabIndex = 67;
+            this.txtposit.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(158, 71);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(153, 22);
+            this.label7.TabIndex = 71;
+            this.label7.Text = "Numero del 1 al 5:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Constantia", 12.22642F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(137, 22);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(457, 22);
+            this.label8.TabIndex = 72;
+            this.label8.Text = "Ingresar 5 numeros separados por coma en cada fila:";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(148, 128);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(164, 22);
+            this.label6.TabIndex = 73;
+            this.label6.Text = "Numero del 6 al 10:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(146, 193);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(165, 22);
+            this.label5.TabIndex = 74;
+            this.label5.Text = "Numero del 11 al 15:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(138, 250);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(173, 22);
+            this.label2.TabIndex = 75;
+            this.label2.Text = "Numero del 16 al 20:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lavender;
+            this.ClientSize = new System.Drawing.Size(707, 545);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtposit);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt16al20);
+            this.Controls.Add(this.txt11al15);
+            this.Controls.Add(this.txt6al10);
+            this.Controls.Add(this.txt1al5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btncerrar);
+            this.Controls.Add(this.btncalc);
+            this.Controls.Add(this.btnlimpiar);
+            this.Controls.Add(this.txtcero);
+            this.Controls.Add(this.txtnegat);
+            this.Font = new System.Drawing.Font("Constantia", 12.22642F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btncerrar;
+        private System.Windows.Forms.Button btncalc;
+        private System.Windows.Forms.Button btnlimpiar;
+        private System.Windows.Forms.TextBox txtcero;
+        private System.Windows.Forms.TextBox txtnegat;
+        private System.Windows.Forms.TextBox txt6al10;
+        private System.Windows.Forms.TextBox txt1al5;
+        private System.Windows.Forms.TextBox txt16al20;
+        private System.Windows.Forms.TextBox txt11al15;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtposit;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Ejercicio 52/Ejercicio 52/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio_52
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btncalc_Click(object sender, EventArgs e)
+        {
+            string[] numerosStr = new string[]
+            {
+                txt1al5.Text, txt6al10.Text, txt11al15.Text, txt16al20.Text
+            };
+
+            double[] numeros = new double[20];
+            int contador = 0;
+
+            foreach (var fila in numerosStr)
+            {
+                string[] numerosFila = fila.Split(',');
+
+                foreach (var numStr in numerosFila)
+                {
+                    if (double.TryParse(numStr, out double num))
+                    {
+                        numeros[contador] = num;
+                        contador++;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Ingresa números válidos separados por comas.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                }
+            }
+
+            int positivos = 0;
+            int negativos = 0;
+            int cero = 0;
+
+            foreach (var num in numeros)
+            {
+                if (num > 0)
+                    positivos++;
+                else if (num < 0)
+                    negativos++;
+                else
+                    cero++;
+            }
+
+            // Mostrar los resultados en los TextBox correspondientes
+            txtposit.Text = positivos.ToString();
+            txtnegat.Text = negativos.ToString();
+            txtcero.Text = cero.ToString();
+        }
+
+        private void btnlimpiar_Click(object sender, EventArgs e)
+        {
+            txt11al15.Clear();
+            txt16al20.Clear();
+            txt1al5.Clear();
+            txt6al10.Clear();
+            txtcero.Clear();
+            txtnegat.Clear();
+            txtposit.Clear();
+        }
+
+        private void btncerrar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
